66b74f2595d8625d5fcbabe007d157dc5a22c5f5
Harsha <harsha@sb-tech-106.local>
2022-12-17 15:38:25 +0530

    Mongo db setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
+  // useEffect(() => {
+  //   axios.get('/api/users').then(
+  //     (response) =>  {
+  //       console.log("respn", response.data);
+  //     }
+  //   )
+  // }, [])
+
+  const addUser = () => {
+    axios.get('/api/users').then(
+      (response) =>  {
+        console.log("respn", response.data);
+      }
+    ).catch(err=> console.log("my error is", err))
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='app'>
+      <button onClick={() => addUser()}>
+
+      </button>
+  </div>
+  )
 }
 
 export default App;
--- server.js
+++ server.js
@@ -1,4 +1,25 @@
 const express = require('express');
-const App = express();
-const PORT = process.env.PORT || 3001;
-App.listen();
+const app = express();
+const { MongoClient} = require('mongodb');
+
+//hosting the mondo uri  -(its the location of the mongo db)
+const mongoUri = "mongodb+srv://Josephy1605:aeqt03Q6xa4Gxw2H@cluster0.h0kkwov.mongodb.net/?retryWrites=true&w=majority";
+
+//Now lets connect to mongo db
+
+MongoClient.connect(mongoUri,{ useNewUrlParser: true,
+    useUnifiedTopology: true }).then((client,err)=>{
+    if(err) {
+        throw(err);
+    }
+    console.log("connected to the db");
+ });
+
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
+
+//username Josephy1605
+//password  aeqt03Q6xa4Gxw2H
+
+// url - mongodb+srv://Josephy1605:<password>@cluster0.h0kkwov.mongodb.net/?retryWrites=true&w=majority
